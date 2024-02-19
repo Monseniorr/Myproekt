f4da720536e58738e4ed3fb1f16dea37ab1540d5
Monseniorr <160451429+Monseniorr@users.noreply.github.com>
2024-02-19 11:54:56 +0800

    Update MyProject2-HTML5-Shipping.data.js

--- MyProject2-HTML5-Shipping.data.js
+++ MyProject2-HTML5-Shipping.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'C:/Goog/MyProject2/Binaries/HTML5/MyProject2-HTML5-Shipping.data';
-    var REMOTE_PACKAGE_BASE = 'MyProject2-HTML5-Shipping.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/qqqqniwbv3apgc7gyeti3/MyProject2-HTML5-Shipping.data?rlkey=v586p7kgb3glttop5ci731zct&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/qqqqniwbv3apgc7gyeti3/MyProject2-HTML5-Shipping.data?rlkey=v586p7kgb3glttop5ci731zct&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
