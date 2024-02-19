ff636dc584f16d2e523191d6fb7d1b196773e226
Monseniorr <160451429+Monseniorr@users.noreply.github.com>
2024-02-19 12:01:28 +0800

    Update MyProject2-HTML5-Shipping.UE4.js

--- MyProject2-HTML5-Shipping.UE4.js
+++ MyProject2-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('MyProject2-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Monseniorr/Myproekt/764c3378bf5b372e089073f64d38b81a65846897/MyProject2-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
